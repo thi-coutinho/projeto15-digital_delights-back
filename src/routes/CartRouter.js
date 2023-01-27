d75f5f3e2f88fc89b981d6520715140f713de9a9
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-27 12:53:28 -0300

    fix:cartController to receive user_id from res.locals.session and created AuthMiddeware

--- src/routes/CartRouter.js
+++ src/routes/CartRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {addProductToCart, delProductCart, getCartProducts} from "../controllers/CartController.js";
+import { authValidation } from "../middlewares/AuthMiddleware.js";
 
 
 const CartRouter = Router()
 
-
+CartRouter.use(authValidation)
 CartRouter.get("/cart",getCartProducts)
 CartRouter.post("/cart",addProductToCart)
 CartRouter.delete("/cart",delProductCart)
