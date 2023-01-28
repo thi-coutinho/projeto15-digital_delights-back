0180f854a466139f363d923a5a1445f9d77233e7
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-28 12:54:34 -0600

    feat: sallesControler done

--- src/controllers/SalesController.js
+++ src/controllers/SalesController.js
@@ -3,31 +3,27 @@ import { cartsCollection, ordersCollection } from "../config/database.js";
 
 //Payment section
 export async function addPaymentMethod(req, res) {
-    const {name, cardNumber, cvv, expDate} = req.body;
-    const {user_id} = res.locals.session;
+    const { name, cardNumber, cvv, expDate } = req.body;
+    const { user_id } = res.locals.session;
     //get user's cart
-    const user = await cartsCollection.findOne({user_id})
-    if (!user) {
+    const userCart = await cartsCollection.findOne({ user_id })
+    if (!userCart) {
         return res.status(404).send("User not found");
     }
-    
-        try {
-            const order = await ordersCollection.insertOne({
-                cart: [{
-                    productId: user.productId, 
-                    quantity: user.quantity 
-                }],
-                payment:[{
-                    name,
-                    cardNumber,
-                    cvv,
-                    expDate
-                }]
-            })
-    
-        return res.status(200).send(order);
 
-        } catch (error) {
-            res.status(500).send(error);
-        }
+    try {
+        const paymentObject = {
+            cart: { ...userCart }, payment: [{
+                name,
+                cardNumber,
+                cvv,
+                expDate
+            }]
+        };
+        const order = await ordersCollection.insertOne(paymentObject);
+        await cartsCollection.deleteOne({ user_id });
+        return res.status(200).send(order);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 }
