1b5d95f7ace0873fdfe976b99479af792b2cd44c
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-27 20:23:40 -0600

    fix: salesController

--- src/controllers/SalesController.js
+++ src/controllers/SalesController.js
@@ -4,18 +4,18 @@ import { cartsCollection, ordersCollection } from "../config/database.js";
 //Payment section
 export async function addPaymentMethod(req, res) {
     const {name, cardNumber, cvv, expDate} = req.body;
-    //const {user_id} = res.locals.session;
+    const {user_id} = res.locals.session;
     //get user's cart
-    //const user = await cartsCollection.findOne({user_id})
-    // if (!user) {
-    //     return res.status(404).send("User not found");
-    // }
+    const user = await cartsCollection.findOne({user_id})
+    if (!user) {
+        return res.status(404).send("User not found");
+    }
     
         try {
             const order = await ordersCollection.insertOne({
                 cart: [{
-                    productId: "teste", 
-                    quantity: "teste" 
+                    productId: user.productId, 
+                    quantity: user.quantity 
                 }],
                 payment:[{
                     name,
